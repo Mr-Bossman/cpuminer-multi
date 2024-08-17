588065b04d5ccae6a1306a8bfeb0add37c68525e
Jesse Taube <Mr.Bossman075@gmail.com>
2024-08-16 21:16:51 -0400

    add verus directory files
    copied directly from https://github.com/monkins1010/ccminer/commit/1ba0bf363fbe96ef2bba02bebeec063fbc62e0b0

---
+++ verus/portability.h
@@ -0,0 +1,29 @@
+#ifndef PORT_H
+#define PORT_H
+#include <immintrin.h>
+#include <x86intrin.h>
+
+typedef __m128i u128;
+
+#ifdef __APPLE__
+#include <sys/types.h>
+#endif// APPLE
+#ifdef _WIN32
+#include <intrin.h>
+#else
+#include <cpuid.h>
+#endif // !WIN32
+
+#include "verus_clhash.h"
+
+
+#ifdef __cplusplus
+#include "uint256.h"
+extern "C"
+{
+#include "haraka.h"
+}
+#else
+#include "haraka.h"
+#endif
+#endif /* PORT_H */
